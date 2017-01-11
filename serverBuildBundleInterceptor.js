aad689f12138d6c4ecb7087f3a47e5f2a822e5a4
yinhf <darkwander.y@gmail.com>
2017-01-11 18:06:58 +0800

    1.测试serverBuildBundleInterceptor  2.android installArchives 0.40.0

--- serverBuildBundleInterceptor.js
+++ serverBuildBundleInterceptor.js
@@ -5,9 +5,12 @@ const path = require('path');
 module.exports = function(options) {
   console.log('serverBuildBundleInterceptor', options);
 
-  return Object.assign({}, options.bundleOptions, {
-    extraNodeModules: {
-      'xxxxx': path.join(__dirname, 'Examples/UIExplorer/js/xxxxx'),
-    }
-  });
+  return {
+    bundleOptions: Object.assign({}, options.bundleOptions, {
+        extraNodeModules: {
+          'xxxxx': path.join(__dirname, 'Examples/UIExplorer/js/xxxxx'),
+        }
+      }),
+    fileChanged: Math.random() > 0.5,
+  };
 };
