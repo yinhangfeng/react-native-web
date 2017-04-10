924f35243b0c8128914e8b705794d717e068b974
yinhf <darkwander.y@gmail.com>
2017-04-10 18:09:57 +0800

    RWLayoutMixin layoutHandle isMounted判断

--- Libraries/lrnw/RWLayoutMixin.web.js
+++ Libraries/lrnw/RWLayoutMixin.web.js
@@ -7,29 +7,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import getLayout from 'RWGetLayout';
 
+function layoutHandle() {
+  let layout = getLayout(ReactDOM.findDOMNode(this));
+  let lastLayout = this.$$lastLayout;
+  if (!lastLayout || lastLayout.x !== layout.x || lastLayout.y !== layout.y || lastLayout.width !== layout.width || lastLayout.height !== layout.height) {
+    this.props.onLayout({nativeEvent: {layout}});
+    this.$$lastLayout = layout;
+  }
+}
+
 //让组件具有onLayout回调功能，在componentDidMount和componentDidUpdate触发判断可能不合理 TODO
-var LayoutMixin = {
+const LayoutMixin = {
 
   componentDidMount: function() {
     if (this.props.onLayout) {
-      setTimeout(this.$$layoutHandle);
+      requestAnimationFrame(() => {
+        if (this.isMounted()) {
+          layoutHandle.call(this);
+        }
+      });
     }
   },
 
   componentDidUpdate: function() {
     if (this.props.onLayout) {
-      this.$$layoutHandle();
+      layoutHandle.call(this);
     }
   },
-
-  $$layoutHandle: function() {
-    let layout = getLayout(ReactDOM.findDOMNode(this));
-    let lastLayout = this.$$lastLayout;
-    if (!lastLayout || lastLayout.x !== layout.x || lastLayout.y !== layout.y || lastLayout.width !== layout.width || lastLayout.height !== layout.height) {
-      this.props.onLayout({nativeEvent: {layout}});
-      this.$$lastLayout = layout;
-    }
-  }
 };
 
 module.exports = LayoutMixin;
