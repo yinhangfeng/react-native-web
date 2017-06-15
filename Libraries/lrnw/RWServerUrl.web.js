ff18a59727185d01cbc9e7fb1104e9a5faab2183
yinhf <darkwander.y@gmail.com>
2017-06-15 11:10:05 +0800

    增加 global.LAB_RESOURCE_BASE_URL 配置

--- Libraries/lrnw/RWServerUrl.web.js
+++ Libraries/lrnw/RWServerUrl.web.js
@@ -3,9 +3,9 @@
  * 资源文件url管理
  */
 
-const PUBLIC_PATH = global.LAB_RESOURCE_PUBLIC_PATH ? LAB_RESOURCE_PUBLIC_PATH : 'rwpublic';
+const PUBLIC_PATH = global.LAB_RESOURCE_PUBLIC_PATH || 'rwpublic';
 //如果全局定义了SOURCE_CODE_SERVER_URL 则使用SOURCE_CODE_SERVER_URL 否则使用当前页面地址
-const serverUrl = global.SOURCE_CODE_SERVER_URL || '';
+const serverUrl = global.SOURCE_CODE_SERVER_URL || global.LAB_RESOURCE_BASE_URL || '';
 
 module.exports = {
   PUBLIC_PATH,
